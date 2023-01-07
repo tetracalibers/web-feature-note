cfb7215009009a6500741766052dc2378c244986
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-01-08 02:26:41 +0900

    gh-pagesでlatex.cssが適用されない問題への対処5

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig(
     },
     integrations: [mdx()],
     site: "https://tetracalibers.github.io",
-    base: "https://tetracalibers.github.io/web-feature-note",
+    base: "web-feature-note",
     experimental: {
       contentCollections: true,
     },
