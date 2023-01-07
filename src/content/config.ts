4583bec82a666ffe04de92c8f1a4f6614e37cbe3
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-01-08 01:11:33 +0900

    CSSノートのスキーマを定義

---
+++ src/content/config.ts
@@ -0,0 +1,29 @@
+import { z, defineCollection } from "astro:content"
+
+const css_modules = defineCollection({
+  schema: {
+    feature_name: z.string().optional(),
+    type: z.enum(["property", "function", "at-rule"]).default("property"),
+    summary: z.string().optional(),
+    external_related_article: z
+      .array(
+        z.object({
+          href: z.string(),
+          label: z.string(),
+        }),
+      )
+      .optional(),
+    inner_related_page: z
+      .array(
+        z.object({
+          href: z.string(),
+          label: z.string(),
+        }),
+      )
+      .optional(),
+  },
+})
+
+export const collections = {
+  "css-modules": css_modules,
+}
