19fbcd437917bd37e37b68c2e25bae135c4a78c0
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-01-09 22:17:07 +0900

    svelte導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,29 @@
 // @ts-check
 
-import { defineConfig } from "astro/config"
-import { resolve } from "node:path"
-import mdx from "@astrojs/mdx"
-const __dirname = new URL(".", import.meta.url).pathname
+import { defineConfig } from "astro/config";
+import { resolve } from "node:path";
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+const __dirname = new URL(".", import.meta.url).pathname;
 
-export default defineConfig(
-  /** @type {import('astro').AstroUserConfig} */
-  {
-    vite: {
-      plugins: [],
-      resolve: {
-        alias: {
-          "@": resolve(__dirname, "src"),
-        },
-      },
-    },
-    integrations: [mdx()],
-    site: "https://tetracalibers.github.io",
-    base: "web-feature-note",
-    experimental: {
-      contentCollections: true,
-    },
-    markdown: {
-      syntaxHighlight: "prism",
-    },
+// https://astro.build/config
+export default defineConfig( /** @type {import('astro').AstroUserConfig} */
+{
+  vite: {
+    plugins: [],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src")
+      }
+    }
   },
-)
+  integrations: [mdx(), svelte()],
+  site: "https://tetracalibers.github.io",
+  base: "web-feature-note",
+  experimental: {
+    contentCollections: true
+  },
+  markdown: {
+    syntaxHighlight: "prism"
+  }
+});
