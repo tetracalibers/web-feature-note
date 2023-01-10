17bf3f6edd6b9b4f3ead6e1356048ddfd8382442
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-01-10 19:28:07 +0900

    md/mdxの一括フォーマット

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,31 @@
 // @ts-check
 
-import { defineConfig } from "astro/config";
-import { resolve } from "node:path";
-import mdx from "@astrojs/mdx";
-import svelte from "@astrojs/svelte";
-const __dirname = new URL(".", import.meta.url).pathname;
+import { defineConfig } from "astro/config"
+import { resolve } from "node:path"
+import mdx from "@astrojs/mdx"
+import svelte from "@astrojs/svelte"
+const __dirname = new URL(".", import.meta.url).pathname
 
 // https://astro.build/config
-export default defineConfig( /** @type {import('astro').AstroUserConfig} */
-{
-  vite: {
-    plugins: [],
-    resolve: {
-      alias: {
-        "@": resolve(__dirname, "src")
-      }
-    }
+export default defineConfig(
+  /** @type {import('astro').AstroUserConfig} */
+  {
+    vite: {
+      plugins: [],
+      resolve: {
+        alias: {
+          "@": resolve(__dirname, "src"),
+        },
+      },
+    },
+    integrations: [mdx(), svelte()],
+    site: "https://tetracalibers.github.io",
+    base: "web-feature-note",
+    experimental: {
+      contentCollections: true,
+    },
+    markdown: {
+      syntaxHighlight: "prism",
+    },
   },
-  integrations: [mdx(), svelte()],
-  site: "https://tetracalibers.github.io",
-  base: "web-feature-note",
-  experimental: {
-    contentCollections: true
-  },
-  markdown: {
-    syntaxHighlight: "prism"
-  }
-});
+)
